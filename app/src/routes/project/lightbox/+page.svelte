62ac935c53998f9390b22dc72c73a271dd8c4e97
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2026-09-24 10:16:47 -0700

    add 5 animation frames to fade labels into the exploded view; add nRF Connect section

--- app/src/routes/project/lightbox/+page.svelte
+++ app/src/routes/project/lightbox/+page.svelte
@@ -1,5 +1,4 @@
 <!-- Lithopane Lightbox -->
-<!-- TODO: mechanical parts and 3D exploded view -->
 
 <script lang="ts">
 	import ImageCols from '$lib/ImageCols.svelte';
@@ -24,9 +23,10 @@
 			width: '50vw'
 		}
 	];
+	import nrfConnect from '$lib/assets/lightbox/nrf-connect.png';
 
 	// exploded view animation frames
-	const frames = new Array(231);
+	const frames = new Array(236);
 	const modules = import.meta.glob('$lib/assets/lightbox/exploded-view/*.webp', {
 		eager: true,
 		import: 'default'
@@ -45,21 +45,21 @@
 	image={titleImg}
 />
 
-
 <div class="mx-8 flex flex-col gap-4 md:mx-40">
+	<!-- scrolly exploded view -->
 	<ScrollyAnimation {frames} scrollDist="250vh" />
-	<p class="translate-y-[-15vh]"></p>
+
 	<h3>Controller PCB</h3>
 	<p>
-		I designed this PCB around an ESP32-S3 SoC. It runs the NimBLE Bluetooth Low-Energy (BLE) stack,
-		responding to on/off events and configuration updates sent from a mobile phone. It advertises
-		itself using the GAP protocol and can be paired to a phone. Then, it listens for GATT events and
-		drives the LEDs (on another board) accordingly.
+		I designed this PCB around an ESP32-S3 SoC. Its firmware is built with the Espressif IoT
+		Development Framework (ESP-IDF) and the NimBLE Bluetooth Low-Energy (BLE) stack. It responds to
+		to on/off events and configuration updates sent from a mobile phone. It implements the GAP
+		protocol to advertise itself pair to a phone. Then, it listens for GATT events and drives the
+		LEDs (on another board) accordingly.
 	</p>
 	<p>
-		I used an RTOS to manage the BLE stack concurrently with driving the LEDs. Responding to GATT
-		events, BLE state management, and button input all happens reliably. Message queues pass
-		commands and data between tasks.
+		I used an RTOS to run the BLE stack and LED drivers concurrently. Message queues pass commands
+		and data between tasks.
 	</p>
 	<p>
 		The ESP32-S3 features a native USB peripheral, so I exposed two GPIOs to a 3-pin header (GND,
@@ -67,13 +67,18 @@
 		board through its USB-C connector. When mounted in its case, only the USB-C connector, along
 		with a button, are exposed.
 	</p>
+	<p>
+		A logic level shifter on the LED output ensures that the signal reliably reaches the first LED
+		on the LED grid. The USB differential data traces are routed close together to maximize their
+		noise immunity.
+	</p>
 	<div class="my-20">
 		<ImageCols images={imageData} />
 	</div>
 
 	<h3>LED Grid</h3>
 	<p>
-		This PCB holds 54 individually-addressable LEDs of the WS2812B chipset. It is receives power and
+		This board has 54 individually-addressable LEDs of the WS2812B chipset. It is receives power and
 		data input from the controller PCB (above), and adds a bulk capacitor. This stabilizes the
 		voltage across flashing LED patterns, and prevents the ICs inside of each LED from browning out
 		and corrupting the daisy-chained data signal.
@@ -83,14 +88,29 @@
 	</div>
 	<p>
 		Rather than route power traces, the front surface has a 5V plane which the LEDs connect to. This
-		"star"-like configuration minimizes voltage drop and ensures each LED has the same voltage to
-		reference the data signal to, ensuring reliable data transfer. This specific LEDs have internal
+		"star"-like configuration minimizes voltage drop and ensures each LED has a similar voltage to
+		reference the data signal to, ensuring reliable data transfer. These LEDs have internal
 		capacitors, so no external decoupling capacitors are needed.
 	</p>
 	<p>
 		The back layer has a ground plane. The data signals are chained between LEDs on the front
 		surface so that their return paths have an uninterrupted ground plane to flow through.
 	</p>
+
+	<h3>Bluetooth</h3>
+	<p>
+		The lightbox can be turned on and off from a mobile phone. It features the Automation IO BLE
+		characteristic, so a paired phone can write to this value to change the state. In the future, it
+		could support color changing functionality and even animations to take full advantage of the
+		individually-addressable RGB LEDs. I haven't gotten around to building a mobile app, so for now
+		I use the nRF Connect BLE testing app from Nordic Semiconductor to control the lightbox.
+	</p>
+	<div class="w-1/2 sm:w-2/5 md:w-1/2 mx-auto">
+		<LargeImage
+			image={nrfConnect}
+			caption="The nRF Connect application can connect, bond, and write to the lightbox"
+		/>
+	</div>
 </div>
 
 <div class="mx-8 md:mx-40"></div>
